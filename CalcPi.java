5a89ddf0c8f3bebac29412503c8f030ca0aab892
Omri Gal <omri.gal01@post.runi.ac.il>
2024-11-13 20:09:33 +0200

    First attempt

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		int count = 0;
+		double sum = 0;
+		double denominator = 1;
+		boolean isPlus = true;
+		while (count < n) {
+			sum = sum + (((isPlus) ? (1) : (-1)) * (1 / denominator));
+			denominator = denominator + 2;
+			isPlus = !isPlus;
+			count = count + 1;
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + sum * 4);
 	}
 }
