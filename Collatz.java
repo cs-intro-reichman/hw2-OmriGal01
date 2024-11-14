6b624229f70dbf41018a7a43ec44423cf33a7d0a
Omri Gal <omri.gal01@post.runi.ac.il>
2024-11-14 08:58:09 +0200

    Renamed int

--- Collatz.java
+++ Collatz.java
@@ -1,14 +1,14 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    int n = Integer.parseInt(args[0]);
+	    int N = Integer.parseInt(args[0]);
 		String mode = args[1];
 		int seed;
 		boolean isV = (mode.equals("v") ? true : false);
 		boolean reachOne = false;
 		int count = 1;
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= N; i++) {
 			//The outer loop, that runs the given number of times.
 			seed = i;
 
@@ -31,7 +31,7 @@ public class Collatz {
 			count = 1;
 			reachOne = false;
 			}
-			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 			//If the loop ended, it means all the sequences have reached 1, so no need to check.
 	}
 }
